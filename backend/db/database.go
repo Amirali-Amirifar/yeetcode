096aa0f2c009e82ccf7f4e823277a11ea2e040b4
hananeh <hananeh.moballegh@tapsi.cab>
2025-04-03 21:34:11 +0330

    update

--- backend/db/database.go
+++ backend/db/database.go
@@ -95,15 +95,3 @@ func Init() *gorm.DB {
 	return DB
 }
 
-func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
-	return string(hashedPassword), nil
-}
-
-func CheckPassword(storedPassword, providedPassword string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(providedPassword))
-	return err == nil
-}
