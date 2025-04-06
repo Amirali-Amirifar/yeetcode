cb9b92149c5c8c444b5a46984596a0e57a8cae1e
Amirali Amirifar <amirali.amirifar@gmail.com>
2025-04-06 12:19:26 +0330

    Add basic front-end setup

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d%02d/%02d", year, month, day)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+	router.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+	router.LoadHTMLGlob("./templates/**/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home", gin.H{})
+	})
+
+	router.Static("/static", "./static")
+	router.Run(":8080")
 }
---
+++ backend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./templates/**/*.{gohtml,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
