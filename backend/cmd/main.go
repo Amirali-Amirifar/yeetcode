6fe2a83b7564d2346b69ce54848a83817bcf87b7
Amirali Amirifar <amirali.amirifar@gmail.com>
2025-04-24 19:22:08 +0330

    refactor front-end

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"time"
-
 	"github.com/Amirali-Amirifar/yeetcode/backend/config"
 	"github.com/Amirali-Amirifar/yeetcode/backend/db"
 	"github.com/Amirali-Amirifar/yeetcode/backend/handlers"
@@ -12,28 +9,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func formatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d%02d/%02d", year, month, day)
-}
-
 func main() {
 	config.GetConfig()
 	db.Init()
 
 	router := gin.Default()
-	router.SetFuncMap(template.FuncMap{
-		"formatAsDate": formatAsDate,
-		"isLoggedIn": func() bool {
-			fmt.Println("isLoggedIn")
-			return true
-		},
-	})
 
 	router.LoadHTMLGlob("templates/**/*")
+	router.Static("/static", "./static")
 
 	handlers.InitHandlers(router)
 
-	router.Static("/static", "./static")
 	router.Run(fmt.Sprintf(":%s", config.GetConfig().ServerPort))
 }
